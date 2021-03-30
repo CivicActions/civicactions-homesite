076e3abb38093aa6a8d169027500c09c11432019
Yodart <joaogabrielgcoutinho@gmail.com>
2021-03-30 17:09:22 -0300

    Implemented review-recommended changes from @iris-i

--- src/pages/services.js
+++ src/pages/services.js
@@ -16,15 +16,17 @@ const ServicesPage = ({ data }) => {
         <div className="inner">
           <h2>Government services that build public trust</h2>
           <div className="body">
-            At its core, digital transformation is about improving the customer
-            experience of government. Public services should be accessible and
-            secure, with human needs at the forefront.
-          </div>
-          <div className="body">
-            Using technology and design as a means to that end, we partner with
-            your agency to help you plan and build modern systems that work
-            efficiently for your staff and the public, increasing trust and
-            satisfaction for those you serve.
+            <p>
+              At its core, digital transformation is about improving the
+              customer experience of government. Public services should be
+              accessible and secure, with human needs at the forefront.
+            </p>
+            <p>
+              Using technology and design as a means to that end, we partner
+              with your agency to help you plan and build modern systems that
+              work efficiently for your staff and the public, increasing trust
+              and satisfaction for those you serve.
+            </p>
           </div>
         </div>
       </section>
