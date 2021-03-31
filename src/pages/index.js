af0fe8ad04bd947acabc11dc7f238d3b1f6ae23e
Yodart <joaogabrielgcoutinho@gmail.com>
2021-03-31 20:45:54 -0300

    Moved the clients section on the homepage to its own component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,10 @@ import "../sass/styles.scss";
 import React, { useEffect, useRef } from "react";
 import Card from "../components/card.js";
 import PrimaryPageCTA from "../components/primary-page-cta.js";
+import ClientsSection from "../components/clients.js";
 import homeIntroGraphic from "../files/images/home-intro-graphic.png";
 import homeTeamImage from "../files/images/home-team-img.png";
 import quotePatternGraphic from "../files/images/quote-pattern-graphic.png";
-import clientLogo1 from "../files/images/client-logos/client-logo-1.png";
-import clientLogo2 from "../files/images/client-logos/client-logo-2.png";
-import clientLogo3 from "../files/images/client-logos/client-logo-3.png";
-import clientLogo4 from "../files/images/client-logos/client-logo-4.png";
-import clientLogo5 from "../files/images/client-logos/client-logo-5.png";
-import clientLogo6 from "../files/images/client-logos/client-logo-6.png";
-import clientLogo7 from "../files/images/client-logos/client-logo-7.png";
-import clientLogo8 from "../files/images/client-logos/client-logo-8.png";
-import clientLogo9 from "../files/images/client-logos/client-logo-9.png";
-import clientLogo10 from "../files/images/client-logos/client-logo-10.png";
-import clientLogo11 from "../files/images/client-logos/client-logo-11.png";
-import clientLogo12 from "../files/images/client-logos/client-logo-12.png";
-import clientLogo13 from "../files/images/client-logos/client-logo-13.png";
-import clientLogo14 from "../files/images/client-logos/client-logo-14.png";
-import clientLogo15 from "../files/images/client-logos/client-logo-15.png";
-import clientLogo16 from "../files/images/client-logos/client-logo-16.png";
-import clientLogo17 from "../files/images/client-logos/client-logo-17.png";
-import clientLogo18 from "../files/images/client-logos/client-logo-18.png";
 import caseStudyTeaserImg1 from "../files/images/case-study-teasers/case-study-teaser-img-1.png";
 import caseStudyTeaserImg2 from "../files/images/case-study-teasers/case-study-teaser-img-2.png";
 import caseStudyTeaserImg3 from "../files/images/case-study-teasers/case-study-teaser-img-3.png";
@@ -37,7 +20,7 @@ import workforceDevelopmentIcon from "../files/icons/workforce-dev-icon.svg";
 import arrowIcon from "../files/icons/arrow-icon.svg";
 import GeneralLayout from "../layouts/general";
 
-const HomePage = ({ data }) => {
+const HomePage = () => {
   const fadersRef = useRef([]);
   useEffect(() => {
     const faders = fadersRef.current;
@@ -71,34 +54,7 @@ const HomePage = ({ data }) => {
           </div>
         </div>
       </section>
-      <section className="home__clients-section">
-        <div className="inner">
-          <div class="body-large">
-            Strategy, design, technology, consulting, and training services for
-            government agencies
-          </div>
-          <div class="grid">
-            <img src={clientLogo1} alt="Client Logo"></img>
-            <img src={clientLogo2} alt="Client Logo"></img>
-            <img src={clientLogo3} alt="Client Logo"></img>
-            <img src={clientLogo4} alt="Client Logo"></img>
-            <img src={clientLogo5} alt="Client Logo"></img>
-            <img src={clientLogo6} alt="Client Logo"></img>
-            <img src={clientLogo7} alt="Client Logo"></img>
-            <img src={clientLogo8} alt="Client Logo"></img>
-            <img src={clientLogo9} alt="Client Logo"></img>
-            <img src={clientLogo10} alt="Client Logo"></img>
-            <img src={clientLogo11} alt="Client Logo"></img>
-            <img src={clientLogo12} alt="Client Logo"></img>
-            <img src={clientLogo13} alt="Client Logo"></img>
-            <img src={clientLogo14} alt="Client Logo"></img>
-            <img src={clientLogo15} alt="Client Logo"></img>
-            <img src={clientLogo16} alt="Client Logo"></img>
-            <img src={clientLogo17} alt="Client Logo"></img>
-            <img src={clientLogo18} alt="Client Logo"></img>
-          </div>
-        </div>
-      </section>
+      <ClientsSection />
       <div className="home__services-and-cases-bg">
         <section className="home__services-section">
           <div className="inner">
