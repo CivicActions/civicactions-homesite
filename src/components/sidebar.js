a201a1a56464613070f683c2f4d44d4a22d9699a
Yodart <joaogabrielgcoutinho@gmail.com>
2021-04-14 15:13:32 -0300

    Fixes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,63 +1,90 @@
-import "../sass/styles.scss";
-import React, { useEffect, useRef } from "react";
-import { Link } from "gatsby";
-import closeIcon from "../files/icons/close-icon.svg";
-import arrowIcon from "../files/icons/ios-arrow-icon.svg";
+import '../sass/styles.scss';
+import React, { useRef } from 'react';
+import { Link } from 'gatsby';
+import closeIcon from '../files/icons/close-icon.svg';
+import arrowIcon from '../files/icons/ios-arrow-icon.svg';
 
 const Sidebar = (props) => {
   const headingsRef = useRef([]);
   const onCollapseClick = (index) => {
     const current = headingsRef.current[index].className;
-    headingsRef.current[index].className = current.includes("collapsed")
-      ? "link"
-      : "link collapsed";
+    headingsRef.current[index].className = current.includes('collapsed')
+      ? 'link'
+      : 'link collapsed';
   };
   return (
     <div>
-      {props.hidden == false ? (
-        <div className="background">
-          <div className="sidebar">
-            <div className="close">
-              <img src={closeIcon} alt="" onClick={props.close}></img>
+      {props.hidden === false ? (
+        <div className='background'>
+          <div className='sidebar'>
+            <div
+              className='close'
+              role='button'
+              aria-label='Close Sidebar'
+              onKeyDown={() => {}}
+              tabIndex={0}
+              onClick={props.close}>
+              <img src={closeIcon} alt='Close Icon'></img>
             </div>
-            <div className="link" ref={(e) => (headingsRef.current[0] = e)}>
-              <div className="heading" onClick={() => onCollapseClick(0)}>
-                <Link to="/">
+            <div
+              className='link'
+              role='button'
+              onKeyDown={() => {}}
+              tabIndex={0}
+              ref={(e) => (headingsRef.current[0] = e)}>
+              <div
+                className='heading'
+                role='button'
+                onKeyDown={() => {}}
+                tabIndex={0}
+                onClick={() => onCollapseClick(0)}>
+                <Link to='/'>
                   <h2>Company</h2>
                 </Link>
-                <img src={arrowIcon} alt=""></img>
+                <img src={arrowIcon} alt=''></img>
               </div>
               <h3>About</h3>
               <h3>Team</h3>
               <h3>Press</h3>
             </div>
-            <div className="divisor"></div>
-            <div className="link">
-              <Link to="/services">
+            <div className='divisor'></div>
+            <div className='link'>
+              <Link to='/services'>
                 <h2>Services</h2>
               </Link>
             </div>
-            <div className="divisor"></div>
-            <div className="link" ref={(e) => (headingsRef.current[1] = e)}>
-              <div className="heading" onClick={() => onCollapseClick(1)}>
-                <Link to="/">
+            <div className='divisor'></div>
+            <div
+              className='link'
+              role='button'
+              onKeyDown={() => {}}
+              tabIndex={0}
+              ref={(e) => (headingsRef.current[1] = e)}>
+              <div
+                tabIndex={0}
+                className='heading'
+                alt=''
+                role='button'
+                onKeyDown={() => {}}
+                onClick={() => onCollapseClick(1)}>
+                <Link to='/'>
                   <h2>Our work</h2>
                 </Link>
-                <img src={arrowIcon} alt=""></img>
+                <img src={arrowIcon} alt=''></img>
               </div>
               <h3>Case studies</h3>
               <h3>Approach</h3>
             </div>
-            <div className="divisor"></div>
-            <div className="link">
+            <div className='divisor'></div>
+            <div className='link'>
               <h2>Insight</h2>
             </div>
-            <div className="divisor"></div>
-            <div className="link">
+            <div className='divisor'></div>
+            <div className='link'>
               <h2>Careers</h2>
             </div>
-            <div className="divisor"></div>
-            <div className="link">
+            <div className='divisor'></div>
+            <div className='link'>
               <h2>Contact</h2>
             </div>
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
-import "../sass/styles.scss";
-import React, { useEffect, useRef } from "react";
-import Card from "../components/card.js";
-import PrimaryPageCTA from "../components/primary-page-cta.js";
-import ClientsSection from "../components/clients.js";
-import homeIntroGraphic from "../files/images/home-intro-graphic.png";
-import homeTeamImage from "../files/images/home-team-img.png";
-import quotePatternGraphic from "../files/images/quote-pattern-graphic.png";
-import caseStudyTeaserImg1 from "../files/images/case-study-teasers/case-study-teaser-img-1.png";
-import caseStudyTeaserImg2 from "../files/images/case-study-teasers/case-study-teaser-img-2.png";
-import caseStudyTeaserImg3 from "../files/images/case-study-teasers/case-study-teaser-img-3.png";
-import caseStudyTeaserImg4 from "../files/images/case-study-teasers/case-study-teaser-img-4.png";
-import caseStudyTeaserImg5 from "../files/images/case-study-teasers/case-study-teaser-img-5.png";
-import dataServicesIcon from "../files/icons/data-services-icon.svg";
-import itModernizationIcon from "../files/icons/it-modernization-icon.svg";
-import productDesignIcon from "../files/icons/product-design-icon.svg";
-import securityComplianceIcon from "../files/icons/security-compliance-icon.svg";
-import webCmsIcon from "../files/icons/web-cms-icon.svg";
-import workforceDevelopmentIcon from "../files/icons/workforce-dev-icon.svg";
-import arrowIcon from "../files/icons/arrow-icon.svg";
-import GeneralLayout from "../layouts/general";
+import '../sass/styles.scss';
+import React, { useEffect, useRef } from 'react';
+import Card from '../components/card.js';
+import PrimaryPageCTA from '../components/primary-page-cta.js';
+import ClientsSection from '../components/clients.js';
+import homeIntroGraphic from '../files/images/home-intro-graphic.png';
+import homeTeamImage from '../files/images/home-team-img.png';
+import quotePatternGraphic from '../files/images/quote-pattern-graphic.png';
+import caseStudyTeaserImg1 from '../files/images/case-study-teasers/case-study-teaser-img-1.png';
+import caseStudyTeaserImg2 from '../files/images/case-study-teasers/case-study-teaser-img-2.png';
+import caseStudyTeaserImg3 from '../files/images/case-study-teasers/case-study-teaser-img-3.png';
+import caseStudyTeaserImg4 from '../files/images/case-study-teasers/case-study-teaser-img-4.png';
+import caseStudyTeaserImg5 from '../files/images/case-study-teasers/case-study-teaser-img-5.png';
+import dataServicesIcon from '../files/icons/data-services-icon.svg';
+import itModernizationIcon from '../files/icons/it-modernization-icon.svg';
+import productDesignIcon from '../files/icons/product-design-icon.svg';
+import securityComplianceIcon from '../files/icons/security-compliance-icon.svg';
+import webCmsIcon from '../files/icons/web-cms-icon.svg';
+import workforceDevelopmentIcon from '../files/icons/workforce-dev-icon.svg';
+import arrowIcon from '../files/icons/arrow-icon.svg';
+import GeneralLayout from '../layouts/general';
 
 const HomePage = () => {
   const fadersRef = useRef([]);
@@ -28,7 +28,7 @@ const HomePage = () => {
       function (entries, appearOnScroll) {
         entries.forEach((entry) => {
           if (!entry.isIntersecting) return;
-          entry.target.classList.add("appear");
+          entry.target.classList.add('appear');
           appearOnScroll.unobserve(entry.target);
         });
       },
@@ -38,184 +38,184 @@ const HomePage = () => {
   }, []);
   return (
     <GeneralLayout>
-      <section className="home__hero-section">
-        <div className="inner">
+      <section className='home--hero-section'>
+        <div className='inner'>
           <img
             src={homeIntroGraphic}
-            alt="Civic Actions Logo"
+            alt='Civic Actions Logo'
             ref={(e) => (fadersRef.current[0] = e)}
-            className="fade-in"></img>
-          <div className="info-text">
+            className='fade-in'></img>
+          <div className='info-text'>
             <h2>
               We help government deliver better public services through modern
               technology and design.
             </h2>
-            <h6 className="primary-button">SEE OUR WORK</h6>
+            <h6 className='primary-button'>SEE OUR WORK</h6>
           </div>
         </div>
       </section>
       <ClientsSection />
-      <div className="home__services-and-cases-bg">
-        <section className="home__services-section">
-          <div className="inner">
+      <div className='home--services-and-cases-bg'>
+        <section className='home--services-section'>
+          <div className='inner'>
             <h2>Digital first. Data driven. Human centered.</h2>
-            <p className="body-large">
+            <p className='body-large'>
               Bringing government services up to today's standards requires new
               ways of thinking and working.
             </p>
-            <p className="body-large">
+            <p className='body-large'>
               We can help you improve how people, process, and technology work
               together at your agency for lasting digital transformation.
             </p>
             <div
-              className="service-cards-grid fade-in"
+              className='service-cards-grid fade-in'
               ref={(e) => (fadersRef.current[1] = e)}>
-              <Card title="Web & CMS" icon={webCmsIcon} />
+              <Card title='Web & CMS' icon={webCmsIcon} />
               <Card
-                title="IT & Service Modernization"
+                title='IT & Service Modernization'
                 icon={itModernizationIcon}
               />
-              <Card title="Product & Design" icon={productDesignIcon} />
+              <Card title='Product & Design' icon={productDesignIcon} />
               <Card
-                title="Security & Compliance"
+                title='Security & Compliance'
                 icon={securityComplianceIcon}
               />
-              <Card title="Data Services" icon={dataServicesIcon} />
+              <Card title='Data Services' icon={dataServicesIcon} />
               <Card
-                title="Workforce Development"
+                title='Workforce Development'
                 icon={workforceDevelopmentIcon}
               />
             </div>
-            <div className="primary-button">EXPLORE SERVICES</div>
+            <div className='primary-button'>EXPLORE SERVICES</div>
           </div>
         </section>
-        <section className="home__case-studies-section">
-          <div className="inner">
+        <section className='home--case-studies-section'>
+          <div className='inner'>
             <h2>Resilient agencies. Accessible services. Happier people.</h2>
-            <p className="body-large">
+            <p className='body-large'>
               Working for the greater good is in our DNA. From healthcare to
               welfare to education and beyond, we partner with agencies to solve
               hard problems and improve outcomes for government and the public.
             </p>
             <div
-              className="grid fade-in"
+              className='grid fade-in'
               ref={(e) => (fadersRef.current[2] = e)}>
-              <div className="cta">
-                <img src={caseStudyTeaserImg1} alt="Client Logo"></img>
-                <div className="img-overlay"> </div>
-                <div className="info">
+              <div className='cta'>
+                <img src={caseStudyTeaserImg1} alt='Client Logo'></img>
+                <div className='img-overlay'> </div>
+                <div className='info'>
                   <h6>CENTERS FOR MEDICARE AND MEDICAID SERVICES</h6>
                   <h4>
                     Improving the online experience for Medicare beneficiaries
                   </h4>
                 </div>
               </div>
-              <div className="cta">
-                <img src={caseStudyTeaserImg2} alt="Client Logo"></img>
-                <div className="img-overlay"> </div>
-                <div className="info">
+              <div className='cta'>
+                <img src={caseStudyTeaserImg2} alt='Client Logo'></img>
+                <div className='img-overlay'> </div>
+                <div className='info'>
                   <h6>US DEPARTMENT OF VETERANS AFFAIRS</h6>
                   <h4>Helping Veterans access care and benefits online</h4>
                 </div>
               </div>
-              <div className="cta">
-                <img src={caseStudyTeaserImg3} alt="Client Logo"></img>
-                <div className="img-overlay"> </div>
-                <div className="info">
+              <div className='cta'>
+                <img src={caseStudyTeaserImg3} alt='Client Logo'></img>
+                <div className='img-overlay'> </div>
+                <div className='info'>
                   <h6>US DEPARTMENT OF EDUCATION</h6>
                   <h4>Supporting and expanding adult education</h4>
                 </div>
               </div>
-              <div className="view-our-work-cta ">
-                <img src={caseStudyTeaserImg3} alt="" className="bg"></img>
-                <div className="content">
+              <div className='view-our-work-cta '>
+                <img src={caseStudyTeaserImg3} alt='' className='bg'></img>
+                <div className='content'>
                   <h3>View more Work</h3>
-                  <img src={arrowIcon} alt=""></img>
+                  <img src={arrowIcon} alt=''></img>
                 </div>
               </div>
             </div>
           </div>
         </section>
       </div>
-      <section className="home__quote-section">
-        <div className="inner">
-          <img src={quotePatternGraphic} alt=""></img>
-          <div className="quote">
+      <section className='home--quote-section'>
+        <div className='inner'>
+          <img src={quotePatternGraphic} alt=''></img>
+          <div className='quote'>
             <h4>
               “It was a pleasure to work with the CivicActions team. Their
               experience with government clients and agile processes helped us
               reach our user base sooner than expected with a product that met
               their needs.”
             </h4>
-            <div className="body">
+            <div className='body'>
               Lisa Berry, Senior GlobalNET Liaison Officer
             </div>
-            <div className="body">Defense Security Cooperation Agency</div>
+            <div className='body'>Defense Security Cooperation Agency</div>
           </div>
         </div>
       </section>
-      <section className="home__learn-w-us-section">
-        <div className="inner">
+      <section className='home--learn-w-us-section'>
+        <div className='inner'>
           <h2>Learn with us.</h2>
-          <div className="body-large">
+          <div className='body-large'>
             Thoughts and takeaways from our work in the field.
           </div>
-          <div className="cases">
-            <div className="grid-item-1">
-              <img src={caseStudyTeaserImg4} alt=""></img>
+          <div className='cases'>
+            <div className='grid-item-1'>
+              <img src={caseStudyTeaserImg4} alt=''></img>
               <h4>Government customer experience: A practical guide</h4>
-              <p className="body">
+              <p className='body'>
                 How to start bringing CX into the business of government
               </p>
             </div>
-            <div className="grid-item-2">
-              <img src={caseStudyTeaserImg5} alt=""></img>
+            <div className='grid-item-2'>
+              <img src={caseStudyTeaserImg5} alt=''></img>
               <h4>WhiteHouse.gov: Beginning an accessibility journey</h4>
-              <p className="body">
+              <p className='body'>
                 Praise and recommendations for the new administration
               </p>
             </div>
-            <div className="grid-item-3">
-              <div className="grid-item-3-1">
+            <div className='grid-item-3'>
+              <div className='grid-item-3-1'>
                 <h4>The role of UX in an agile team</h4>
-                <img width="32px" src={arrowIcon} alt=""></img>
+                <img width='32px' src={arrowIcon} alt=''></img>
               </div>
-              <div className="grid-item-3-2">
+              <div className='grid-item-3-2'>
                 <h4>What “fierce openness” can do for government</h4>
-                <img width="32px" src={arrowIcon} alt=""></img>
+                <img width='32px' src={arrowIcon} alt=''></img>
               </div>
             </div>
           </div>
-          <div className="primary-button">
+          <div className='primary-button'>
             <h6>EXPLORE POSTS & VIDEOS</h6>
           </div>
         </div>
       </section>
-      <section className="home__team-section">
-        <div className="inner">
-          <div className="column">
+      <section className='home--team-section'>
+        <div className='inner'>
+          <div className='column'>
             <h2>Our people make the difference.</h2>
-            <p className="body-large">
+            <p className='body-large'>
               We are leaders in civic tech and design, committed to working in
               ways that make life better for our clients and each other.
             </p>
-            <div className="primary-button">
+            <div className='primary-button'>
               <h6>MEET OUR TEAM</h6>
             </div>
           </div>
-          <div className="team-picture-column">
-            <img src={homeTeamImage} alt="Team Picture"></img>
-            <div className="primary-button">
+          <div className='team-picture-column'>
+            <img src={homeTeamImage} alt=''></img>
+            <div className='primary-button'>
               <h6>MEET OUR TEAM</h6>
             </div>
           </div>
         </div>
       </section>
       <PrimaryPageCTA
-        title="Let’s build a public success story."
-        subtitle="Get in touch to start."
-        primaryButtonText="HIRE US"
-        secondaryButtonText="EXPLORE SERVICES"
+        title='Let’s build a public success story.'
+        subtitle='Get in touch to start.'
+        primaryButtonText='HIRE US'
+        secondaryButtonText='EXPLORE SERVICES'
       />
     </GeneralLayout>
   );
