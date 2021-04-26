cab4cb6377e159fa9ce6edecd67d5f9b6b3771d7
Yodart <joaogabrielgcoutinho@gmail.com>
2021-04-26 16:11:34 -0300

    Initial Implementation

---
+++ src/components/staff-quote.js
@@ -0,0 +1,29 @@
+import '../sass/styles.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import quotePatternGraphic from '../files/images/quote-pattern-graphic.png';
+
+const StaffQuote = ({ img, quote, name, role }) => {
+  return (
+    <div className='staff-quote'>
+      <div className='inner'>
+        <img src={img} alt='' className='picture'></img>
+        <div className='quote'>
+          <h4>{quote}</h4>
+          <div className='body'>{name}</div>
+          <div className='body'>{role}</div>
+        </div>
+        <img src={quotePatternGraphic} alt='' className='pattern'></img>
+      </div>
+    </div>
+  );
+};
+
+export default StaffQuote;
+
+StaffQuote.propTypes = {
+  img: PropTypes.any,
+  quote: PropTypes.string,
+  name: PropTypes.string,
+  role: PropTypes.string,
+};
