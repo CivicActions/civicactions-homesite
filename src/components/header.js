f71dd27dcdfa33a03cdfaba156b6ad2991239a87
Iris Ibekwe <iris.ibekwe@civicactions.com>
2021-05-03 10:20:57 -0400

    Add a link to the careers page to the top and bottom menus.

--- src/components/header.js
+++ src/components/header.js
@@ -12,19 +12,19 @@ const Header = ({ onMenuClick }) => {
         <img src={extendedLogo} alt='Civic Actions Logo'></img>
       </Link>
       <div className='header-nav'>
-        <Link to=''>company</Link>
-        <Link to='/services'>services</Link>
-        <Link to=''>our work</Link>
-        <Link to=''>insights</Link>
-        <Link to=''>careers</Link>
-        <div className='primary-button'>contact us</div>
+        <Link to=''>Company</Link>
+        <Link to='/services'>Services</Link>
+        <Link to=''>Our work</Link>
+        <Link to=''>Insights</Link>
+        <Link to='/careers'>Careers</Link>
+        <div className='primary-button'>Contact us</div>
       </div>
       <div
         className='header-nav-mobile'
         onClick={onMenuClick}
         role='button'
         tabIndex={0}
-        onKeyDown={() => {}}>
+        onKeyDown={() => { }}>
         <img src={menuIcon} alt=''></img>
       </div>
     </header>
