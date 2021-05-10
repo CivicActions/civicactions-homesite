a9b18317b1d37180f0b41fff2b57038ee89edb72
Alex Scott <alex.scott@civicactions.com>
2021-05-10 14:36:58 +0200

    HOMER-20297: Adding a TeaserGrid component and updating team elements.

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,41 +1,94 @@
 import '../sass/styles.scss';
-import React from 'react';
+import React, {useState} from 'react';
 import GeneralLayout from '../layouts/general';
+import TeaserGrid from '../components/teaser-grid.js';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const TeamPage = () => {
-  const data = useStaticQuery(query);
+
+  const data = useStaticQuery(graphql`
+      {
+        allStrapiStaffProfile {
+          nodes {
+            Name
+            Role
+            Path
+            Image {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 264
+                  height: 264
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+          }
+        }
+      }
+    `);
+
+
+  const teamDataNodes = data.allStrapiStaffProfile.nodes;
+  const [filterState, setFilterState] = useState(null);
+
+  const callback = (e) => {
+    if (e.target.textContent !== "undefined") {
+      setFilterState(e.target.textContent);
+    }
+  };
+  let team;
+  if (filterState) {
+    team = teamDataNodes.filter((person) => person.Role === filterState);
+  } else {
+    team = teamDataNodes;
+  }
   return (
     <GeneralLayout>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
+      <div className="team-page">
+        <section className="placeholder">
+          <div className="">
+            <div>
+              <h2>You've never met a team like this one</h2>
+              <p className="body">Lorem ipsum</p>
+            </div>
+          </div>
+        </section>
+
+        <div class="inner">
+          <FilterButtons parentCallback={callback} team={teamDataNodes} />
+          <p />
+          <div className="section__teaser-grid">
+            <TeamTeasers team={team} />
+          </div>
+        </div>
+      </div>
     </GeneralLayout>
   );
 };
 
-export const query = graphql`
-  {
-    allStrapiStaffProfile {
-      nodes {
-        id
-        Name
-        Pronunciation
-        Personal_Pronouns
-        Role
-        Path
-        Location
-        Quote
-        Social {
-          Title
-          Url
-        }
-        Specialty {
-          Specialty
-          id
-        }
-        Body
-      }
-    }
-  }
-`;
+
+const TeamTeasers = ({ team }) => {
+  return team.map((person, index) => {
+    const { Path, Name, Role, Image } = person;
+    return (<TeaserGrid image={Image} name={Name} link={Path} title={Role} />);
+  });
+};
+
+const FilterButtons = ({ team, parentCallback }) => {
+  const roles = [...new Set(team.map((item) => item.Role))];
+  return roles.map((role, index) => {
+    return (
+      <button
+        key={index}
+        onClick={(e) => {
+          parentCallback(e);
+        }}
+      >
+        {role}
+      </button>
+    );
+  });
+};
 
 export default TeamPage;
