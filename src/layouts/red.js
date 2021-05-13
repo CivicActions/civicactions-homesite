e35bc5b4962762fa350d4b5a3eb838fab3f374ea
Jennifer Aube <jenniferaube0510@gmail.com>
2021-05-13 14:35:18 -0400

    Homer-378 Header red diagonal with accessibility fixes to lang attr

---
+++ src/layouts/red.js
@@ -0,0 +1,39 @@
+import '../sass/styles.scss';
+import React, { useState } from 'react';
+import Banner from '../components/banner.js';
+import Header from '../components/red-header.js';
+import Footer from '../components/footer.js';
+import Sidebar from '../components/sidebar.js';
+import redRectangle from "../files/images/red-rectangle.svg";
+import { Helmet } from "react-helmet"
+
+const RedLayout = ({ children }) => {
+    const [hideSidebar, setHideSidebar] = useState(true);
+    let openSidebar = () => setHideSidebar(false);
+    let closeSidebar = () => setHideSidebar(true);
+    
+    return (
+        <div style={{ position: 'relative' }}>
+            <Helmet
+                htmlAttributes={{
+                    lang: 'en',
+                }}
+            />
+            <div>
+                <Banner
+                    boldText='We’re hiring! '
+                    regularText='View open positions here'
+                />
+                <img className='red-rectangle' src={redRectangle} alt='background red color'></img>
+                <Header onMenuClick={openSidebar} mobileMenuHiddenBool={hideSidebar} />
+                <main>
+                    <div>{children}</div>
+                </main>
+                <Footer />
+            </div>
+            <Sidebar hidden={hideSidebar} close={closeSidebar} />
+        </div>
+    );
+};
+
+export default RedLayout;
