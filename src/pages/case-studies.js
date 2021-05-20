1ba9ed24f160ea03b684d83d08855ae81ea7ab85
Jennifer Aube <jenniferaube0510@gmail.com>
2021-05-20 14:09:52 -0400

    Homer-384 Adding hero to case studies landing page

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -3,11 +3,16 @@ import React from 'react';
 import RedLayout from '../layouts/red';
 import { graphql, useStaticQuery } from 'gatsby';
 import {Helmet} from "react-helmet";
+import Hero from '../components/hero.js';
 
 const CaseStudyPage = () => {
   const data = useStaticQuery(query);
   return (
     <RedLayout>
+        <Hero
+            title='Work that makes a difference'
+            description='Our work impacts the daily lives of millions of people. See how we’ve helped agencies build resilient services at scale.'
+        />
         <Helmet>
             <title data-react-helmet="true">Case Study Landing page</title>
         </Helmet>
