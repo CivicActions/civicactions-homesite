651941cb5b4eed09709b1f6e2b3de0ea6c81eb6a
Iris Ibekwe <iris.ibekwe@civicactions.com>
2021-06-07 09:09:32 -0400

    HOMER-450: Display the clicked team member's bio in the modal.

--- src/templates/offering.js
+++ src/templates/offering.js
@@ -1,204 +1,234 @@
-import React from 'react';
-import {graphql} from 'gatsby';
+import React, { useState } from 'react';
+import { graphql } from 'gatsby';
 import ReactMarkdown from "react-markdown";
+import Modal from 'react-modal';
+import { Helmet } from "react-helmet";
+import { GatsbyImage, getImage } from "gatsby-plugin-image"; import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Components
 import RedLayout from '../layouts/red';
-import {Helmet} from "react-helmet";
 import Hero from "../components/hero-with-buttons";
 import Quote from "../components/quote";
 import PrimaryPageCTA from "../components/ditap-page-cta";
-import {GatsbyImage, getImage} from "gatsby-plugin-image";
 import Tabs from "../components/Tabs";
 import LinkButton from "../components/link-button";
-import {
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-} from 'react-accessible-accordion';
+import Bio from '../components/offering/bio'
 import linkedinIcon from "../files/icons/linkedin.svg";
 import squareCircle from "../files/icons/square-circle.svg";
-import Modal from 'react-modal';
 
-const OfferingTemplate = ({data}) => {
-    const offering = data.allStrapiOffering.nodes[0];
-    console.log(offering);
-    const [modalIsOpen,setIsOpen] = React.useState(false);
-    function openModal(e) {
-        console.log(e);
-        setIsOpen(true);
-    }
-    function closeModal(){
-        setIsOpen(false);
-    }
+const OfferingTemplate = ({ data }) => {
+  Modal.setAppElement('#___gatsby')
 
-    return (
-        <RedLayout>
-            <Helmet>
-                <title data-react-helmet="true">{offering.Title}</title>
-            </Helmet>
-            <div className='offering--content-type'>
-                <Hero
-                    title={offering.Title}
-                    description={offering.Body}
-                    button={offering.hero_button}
-                />
+  const offering = data.allStrapiOffering.nodes[0];
 
-            <section className='section--offering--client-logos'>
-                <div className='inner'>
-                    <h2 className='body'>{offering.client_logo.text}</h2>
-                    <div className='grid'>
-                    {offering.client_logo.client_logo.map((img, index) => (
-                        <img src={img.url} alt={img.alternativeText}/>
-                    ))}
-                    </div>
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalRef, setModalRef] = useState(false);
 
-                </div>
-            </section>
-            {offering.Stats.length &&
-            <section className='section--offering--stats'>
-                <div className='stats--wrapper'>
-                    <div className='inner'>
-                        {offering.Stats.map((stat, index) => (
-                            <div className='single-stat'>
-                                <p className='stat--number'>{offering.Stats[index].Numerical_Element}</p>
-                                <p className='body stat--text'>{offering.Stats[index].Content_Element}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section> }
+  // Initialize the modal content.
+  let modalContent
 
-            <section className='section--offering--value-props'>
-                <div className='inner'>
-                    <div className='value-props--left'>
-                        <h2>{offering.value_prop.header_text}</h2><img className='icon' src={squareCircle} alt=''/>
-                        <ReactMarkdown className='body' children={offering.value_prop.body_text}/>
+  function openModal(currentMemberName) {
+    setIsOpen(true);
+    // Set the clicked member name in state.
+    setModalRef(currentMemberName)
+  }
 
-                    </div>
-                    <div className='value-props--right'>
-                        <img src={offering.value_prop.image[0].url} alt={offering.value_prop.image[0].alternativeText}/>
-                        <caption>{offering.value_prop.image[0].caption}</caption>
-                    </div>
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  // If the modal is open, load the bio information for the clicked team member.
+  if (modalIsOpen === true) {
+    modalContent = offering.team_members.filter((item) => {
+      if (item.Name === modalRef) {
+        return true;
+      };
+    })
+  }
+
+  //=== Get the list of all team members. (@todo: Place in a new component) ====
+  let teamMemberList = offering.team_members.length ?
+    offering.team_members.map((member, index) => (
+
+      <div key={member.id} className='related-staff'>
+        <img className='staff-image' src={member.image[0].url} alt={member.image[0].alternativeText} />
+        <div className='staff-info'>
+          <p className='h3 staff-name'>
+            {member.Name}
+            <a className='linkedin-icon' href={member.Linkedin}><img src={linkedinIcon} /></a>
+          </p>
+          <p className='body staff-role'>{member.Role}</p>
+          <button className={`body open-modal--btn ${index}`} onClick={() => openModal(member.Name)}>Read bio</button>
+        </div></div>
 
 
-                </div>
-            </section>
+    )) : null;
+
+  return (
+    <RedLayout>
+      <Helmet>
+        <title data-react-helmet="true">{offering.Title}</title>
+      </Helmet>
+      <div className='offering--content-type'>
+        <Hero
+          title={offering.Title}
+          description={offering.Body}
+          button={offering.hero_button}
+        />
+
+        <section className='section--offering--client-logos'>
+          <div className='inner'>
+            <h2 className='body'>{offering.client_logo.text}</h2>
+            <div className='grid'>
+              {offering.client_logo.client_logo.map((img) => (
+                <img key={img.id} src={img.url} alt={img.alternativeText} />
+              ))}
+            </div>
+
+          </div>
+        </section>
+        {offering.Stats.length &&
+          <section className='section--offering--stats'>
+            <div className='stats--wrapper'>
+              <div className='inner'>
+                {offering.Stats.map((stat, index) => (
+                  <div className='single-stat'>
+                    <p className='stat--number'>{offering.Stats[index].Numerical_Element}</p>
+                    <p className='body stat--text'>{offering.Stats[index].Content_Element}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>}
+
+        <section className='section--offering--value-props'>
+          <div className='inner'>
+            <div className='value-props--left'>
+              <h2>{offering.value_prop.header_text}</h2><img className='icon' src={squareCircle} alt='' />
+              <ReactMarkdown className='body' children={offering.value_prop.body_text} />
+
+            </div>
+            <div className='value-props--right'>
+              <img src={offering.value_prop.image[0].url} alt={offering.value_prop.image[0].alternativeText} />
+              <caption>{offering.value_prop.image[0].caption}</caption>
+            </div>
+
+
+          </div>
+        </section>
             // TODO Tabs
             <section className='section--offering--tabs'>
-                <div className='inner'>
-                    {offering.tabs.map((tab, index) => (
-                        <div>
-                            <h3>{tab.tab_header}</h3>
-                            {/*<Tabs>*/}
-                            {/*// Tabs component comes from https://www.digitalocean.com/community/tutorials/react-tabs-component*/}
-                            {/*    {tab.tabs_section.map((section, index) => (*/}
-                            {/*        <div label={section.header}>*/}
-                            {/*            <ReactMarkdown className='body' children={section.body}/>*/}
-                            {/*        </div>*/}
-                            {/*    ))}*/}
+          <div className='inner'>
+            {offering.tabs.map((tab, index) => (
+              <div>
+                <h3>{tab.tab_header}</h3>
+                {/*<Tabs>*/}
+                {/*// Tabs component comes from https://www.digitalocean.com/community/tutorials/react-tabs-component*/}
+                {/*    {tab.tabs_section.map((section, index) => (*/}
+                {/*        <div label={section.header}>*/}
+                {/*            <ReactMarkdown className='body' children={section.body}/>*/}
+                {/*        </div>*/}
+                {/*    ))}*/}
 
-                            {/*    <div label={tab.cta_tab.header}>*/}
-                            {/*        <LinkButton text={tab.cta_tab.button_text}*/}
-                            {/*        src={tab.cta_tab.button_link}/>*/}
-                            {/*    </div>*/}
-                            {/*</Tabs>*/}
-                        </div>
-                    ))}
-                </div>
-            </section>
+                {/*    <div label={tab.cta_tab.header}>*/}
+                {/*        <LinkButton text={tab.cta_tab.button_text}*/}
+                {/*        src={tab.cta_tab.button_link}/>*/}
+                {/*    </div>*/}
+                {/*</Tabs>*/}
+              </div>
+            ))}
+          </div>
+        </section>
 
-            {offering.Quote[0] && <Quote classes='staff-quote--first'
-                quote={offering.Quote[0].Quote}
-                source={offering.Quote[0].Source}
-            /> }
-
-            {offering.team_members.length &&
-            <section className='section--offering--staff'>
-                <div className='inner'>
-                    <h2>Meet the team</h2>
-                    <div className='related-staff--wrapper'>
-                        {offering.team_members.map((member, index) => (
-                            <div className='related-staff'>
-                                <img className='staff-image' src={offering.team_members[index].image[0].url} alt={offering.team_members[index].image[0].alternativeText}/>
-                                <div className='staff-info'>
-                                    <p className='h3 staff-name'>
-                                        {offering.team_members[index].Name}
-                                        <a className='linkedin-icon' href={offering.team_members[index].Linkedin}><img src={linkedinIcon}/></a>
-                                    </p>
-                                    <p className='body staff-role'>{offering.team_members[index].Role}</p>
-                                    <button className={`body open-modal--btn ${index}`} onClick={openModal}>Read bio</button>
-                                </div>
-                                <Modal
-                                    isOpen={modalIsOpen}
-                                    onRequestClose={closeModal}
-                                    contentLabel="Staff member modal"
-                                    overlayClassName="Overlay"
-                                    aria-label="staff-modal-biography">
-                                    <img className='staff-image' src={offering.team_members[index].image[0].url} alt={offering.team_members[index].image[0].alternativeText}/>
-                                    <h2 className='h3 staff-name'>{offering.team_members[index].Name}</h2>
-                                    <a className='linkedin-icon' href={offering.team_members[index].Linkedin}><img src={linkedinIcon}/></a>
-                                    <p className='body staff-role'>{offering.team_members[index].Role}</p>
-
-                                    <button onClick={closeModal}>close</button>
-                                    <div><p className='body staff-body'>{offering.team_members[index].Body}</p></div>
-                                </Modal>
-
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>}
-
-            {offering.text_section.Header &&
-            <section className='section--offering--text'>
-                <div className='background-mint'></div>
-                <div className='inner'>
-
-                    <h2>{offering.text_section.Header}</h2>
-                    <ReactMarkdown className='body' children={offering.text_section.body}/>
-                    {offering.text_section.button.map((btn, index) => (
-                        <LinkButton
-                            text={btn.button_text}
-                            src={btn.button_link}
-                        />
-                    ))}
-
-                </div>
-            </section> }
-            {/*// Accordion comes from https://www.npmjs.com/package/react-accessible-accordion*/}
-            {offering.FAQ_Accordion_Section.list_questions.length &&
-            <Accordion id='faq'>
-                {offering.FAQ_Accordion_Section.list_questions.map((faq, index) => (
-                    <AccordionItem>
-                        <AccordionItemHeading>
-                            <AccordionItemButton>
-                                {faq.question}
-                            </AccordionItemButton>
-                        </AccordionItemHeading>
-                        <AccordionItemPanel>
-                            <ReactMarkdown className='body' children={faq.body}/>
-                        </AccordionItemPanel>
-                    </AccordionItem>
-                ))}
-
-            </Accordion>}
-
-            {offering.Quote[1] && <Quote
-                quote={offering.Quote[1].Quote}
-                source={offering.Quote[1].Source}
-            /> }
-
-            <PrimaryPageCTA
-                title={offering.CTA.Header}
-                subtitle={offering.CTA.body}
-                buttons={offering.CTA.cta_button}
-            />
+        {offering.Quote[0] && <Quote classes='staff-quote--first'
+          quote={offering.Quote[0].Quote}
+          source={offering.Quote[0].Source}
+        />}
 
 
-</div>
-        </RedLayout>
-    );
+        {offering.team_members.length &&
+          <section className='section--offering--staff'>
+            <div className='inner'>
+              <h2>Meet the team</h2>
+              <div className='related-staff--wrapper'>
+                {teamMemberList}
+              </div>
+            </div>
+
+            {/* 
+              Load just one Modal wrapper per page and update its content with the
+              clicked team member's bio.
+             */}
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              contentLabel="Staff member modal"
+              overlayClassName="Overlay"
+              aria-label="staff-modal-biography"
+            >
+
+              <Bio member={modalContent} closeModal={closeModal} />
+              <button onClick={closeModal}>close</button>
+
+            </Modal>
+          </section>}
+
+
+        {offering.text_section.Header &&
+          <section className='section--offering--text'>
+            <div className='background-mint'></div>
+            <div className='inner'>
+
+              <h2>{offering.text_section.Header}</h2>
+              <ReactMarkdown className='body' children={offering.text_section.body} />
+              {offering.text_section.button.map((btn, index) => (
+                <LinkButton
+                  text={btn.button_text}
+                  src={btn.button_link}
+                />
+              ))}
+
+            </div>
+          </section>}
+        {/*// Accordion comes from https://www.npmjs.com/package/react-accessible-accordion*/}
+        {offering.FAQ_Accordion_Section.list_questions.length &&
+          <Accordion id='faq'>
+            {offering.FAQ_Accordion_Section.list_questions.map((faq, index) => (
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    {faq.question}
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <ReactMarkdown className='body' children={faq.body} />
+                </AccordionItemPanel>
+              </AccordionItem>
+            ))}
+
+          </Accordion>}
+
+        {offering.Quote[1] && <Quote
+          quote={offering.Quote[1].Quote}
+          source={offering.Quote[1].Source}
+        />}
+
+        <PrimaryPageCTA
+          title={offering.CTA.Header}
+          subtitle={offering.CTA.body}
+          buttons={offering.CTA.cta_button}
+        />
+
+
+      </div>
+    </RedLayout>
+  );
 };
 
 export const query = graphql`
@@ -206,6 +236,7 @@ export const query = graphql`
 query offeringQuery {
   allStrapiOffering {
     nodes {
+      id
       Body
       CTA {
         Header
@@ -232,6 +263,7 @@ query offeringQuery {
           url
           alternativeText
           caption
+          id
         }
       }
       hero_button {
