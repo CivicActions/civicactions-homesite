d69cc9db3478b3fc3c573362650c5b7b68c6a004
Iris Ibekwe <iris.ibekwe@civicactions.com>
2021-06-09 15:43:09 -0400

    HOMER-477: Correct alt text typos.

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,11 +9,11 @@ const NotFoundPage = () => {
   return (
     <GeneralLayout>
 
-        <div className="not-found">
+      <div className="not-found">
         <title>Page Not found</title>
         <h1>404</h1>
         <h2>Well, that's awkward.</h2>
-        <img alt="Funny photo of CIvicActions frozen in a Zoom cal" src={FrozenImg} />
+        <img alt="Funny photo of CivicActions frozen in a Zoom call" src={FrozenImg} />
         <div className="not-found--body">
           <p className="body">We’re frozen on Zoom and can’t find the page you’re looking for.</p>
           <p className="body">While we sign back on, here are some quick links for you:</p>
@@ -29,11 +29,11 @@ const NotFoundPage = () => {
             </p>
           </div>
         </div>
-        </div>
+      </div>
 
       <PrimaryPageCTA title="Let’s build a public success story." subtitle="Get in touch to start." primaryButtonText="PUT US TO WORK" secondaryButtonText="JOIN OUR TEAM" />
 
-      </GeneralLayout>
+    </GeneralLayout>
   );
 };
 
