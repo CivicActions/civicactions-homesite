162e2798277d991502d97a6c82980de0687671b0
Iris Ibekwe <iris.ibekwe@civicactions.com>
2021-07-09 09:26:09 -0400

    HOMER-551: Pull in OGImages for offerings from strapi.

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,24 +4,24 @@ import { Helmet } from 'react-helmet';
 import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
 
-
 const SEO = ({ title, description, image, article }) => {
   const { pathname } = useLocation();
   const { site } = useStaticQuery(query);
   const {
     defaultTitle,
-    titleTemplate,
     defaultDescription,
     siteUrl,
     defaultImage,
     twitterUsername,
   } = site.siteMetadata;
+
   const seo = {
     title: (`${title || defaultTitle} | CivicActions`),
     description: description || defaultDescription,
-    image: image || `${siteUrl}${defaultImage}`,
+    image: `${siteUrl}${image}` || `${siteUrl}${defaultImage}`,
     url: `${siteUrl}${pathname}`,
   };
+
   return (
     <Helmet
       htmlAttributes={{ lang: 'en' }}
@@ -49,18 +49,21 @@ const SEO = ({ title, description, image, article }) => {
   );
 };
 export default SEO;
+
 SEO.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   image: PropTypes.string,
   article: PropTypes.bool,
 };
+
 SEO.defaultProps = {
   title: null,
   description: null,
   image: null,
   article: false,
 };
+
 const query = graphql`
   query SEO {
     site {
