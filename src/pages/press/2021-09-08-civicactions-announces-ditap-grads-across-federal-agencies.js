d3f49bde570f13fb58685024ea6b30ff47ce4da6
Melinda Burgess <melinda.d.burgess@gmail.com>
2021-09-08 17:22:20 -0400

    Update and rename civicactions-announces-ditap-grads-across-federal-agencies.js to 2021-09-08-civicactions-announces-ditap-grads-across-federal-agencies.js

--- src/pages/press/2021-09-08-civicactions-announces-ditap-grads-across-federal-agencies.js
+++ src/pages/press/2021-09-08-civicactions-announces-ditap-grads-across-federal-agencies.js
@@ -5,7 +5,7 @@ import SEO from "../../components/seo";
 import Hero from '../../components/hero.js';
 import PrimaryPageCTA from "../../components/primary-page-cta";
 
-const VADITAPGRAD = () => {
+const DITAPSEPT2021 = () => {
 
     return (
         <RedLayout>
@@ -28,33 +28,54 @@ const VADITAPGRAD = () => {
                     <p className='body'>DITAP also enables contracting officers to be business advisors and change agents within their own agencies. This serves DITAP’s bigger goal of building a culture of innovative acquisition across the federal government.</p>
                     <p className='body'>Upon DITAP graduation, students earn their Federal Acquisition Certification in Contracting Core-Plus Specialization in Digital Services (<a href='https://www.whitehouse.gov/wp-content/uploads/2018/05/fac_c_digital_services_05_12_18.pdf?utm_medium=email&utm_source=govdelivery'>FAC-C-DS</a>). By 2022, the FAC-C-DS will be required for all federal contracting officers who manage contracts of $7M or more.</p>
                     <p className='body'>With this cohort, CivicActions is pleased to congratulate the following DITAP graduates, which are listed by their agency:</p>
+                    <br/>
+                    <h3>Department of Transportation (DOT)</h3>
                     <p className='body'><ul>
-                            <li>Anthony Browner, Contract Specialist</li>
-                            <li>Camille Dye, Contract Specialist</li>
-                            <li>Chiffon Bradshaw, Procurement Analyst</li>
-                            <li>Debra Bryce-Davis, Contract Specialist</li>
-                            <li>Garret Holland, Contract Specialist</li>
-                            <li>Ioannis Vardouniotis, Contract Specialist</li>
-                            <li>James Cummiskey, Contract Specialist</li>
-                            <li>James Morgan, Contract Specialist</li>
-                            <li>John McDonnell, Contract Specialist</li>
-                            <li>Joshua Hammill, Contract Officer</li>
-                            <li>Kalil O'Neal, Contract Specialist</li>
-                            <li>Kevin Andujar, Contract Specialist</li>
-                            <li>Mary Accomando, Supervisory Contracting Officer</li>
-                            <li>Mary Beth Hall, Contract Specialist</li>
-                            <li>Michelle Policari, Contract Specialist</li>
-                            <li>Mina Awad, Contract Specialist</li>
-                            <li>Rene Brown, Procurement Analyst</li>
-                            <li>Robert Robinson, Contract Specialist</li>
-                            <li>Sarah Bach, Contract Specialist</li>
-                            <li>Sharon Haywood, Contract Specialist</li>
-                            <li>Shreeia Curry, Contract Specialist</li>
-                            <li>Susan Meng, Management Analyst</li>
-                            <li>Tasha Cheramie, Contract Specialist</li>
-                        <li>Yash Dega, Contract Specialist</li>
-                        </ul>
-                    </p>
+                            <li>Kyle Griggs, Contract Specialist</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>Environmental Protection Agency (EPA)</h3>
+                    <p className='body'><ul>
+                            <li>Christopher Davis, Contracting Officer</li>
+                            <li>Darren Gann, Contracting Officer</li>
+                            <li>Rodney-Daryl Jones, Director, Information Technology Acquisition</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>Federal Energy Regulatory Commission (FERC)</h3>
+                    <p className='body'><ul>
+                            <li>Yolanda Cooper, Contracting Officer</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>Health and Human Services (HHS)</h3>
+                    <p className='body'><ul>
+                            <li>Jennifer Browning, Branch Chief</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>Social Security Administration (SSA)</h3>
+                    <p className='body'><ul>
+                            <li>Katie Cooney, Division Director, IT Hardware and Maintenance</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>National Nuclear Security Administration (NNSA)</h3>
+                    <p className='body'><ul>
+                            <li>Yolanda Robinson-Freeman, Manager, Specialty Acquisition Branch</li>
+                            <li>Ernesto Jayme, Contract Specialist</li>
+                          </ul>
+                          </p>
+                    <br/>
+                    <h3>United States Department of Agriculture (USDA)</h3>
+                    <p className='body'><ul>
+                            <li>Matthew Carnes, Contracting Officer</li>
+                            <li>Robert Cortez, Contracting Officer</li>
+                            <li>Scott Beckstrand, Chief, Strategic Acquisitions Division</li>
+                            <li>Stuart	Christianson, Contract Specialist</li>
+                          </ul>
+                          </p>
                     <p className='body'>CivicActions is proud to be the nation’s only small business that is fully-authorized  to provide DITAP training. For more information about our DITAP program, visit <a href='https://civicactions.com/services/ditap'>civicactions.com/services/ditap</a>. If you’d like to request training, please <a href='https://civicactions.com/services/ditap/register'>submit your request here</a>.</p>
                 </div>
             </div>
@@ -72,4 +93,4 @@ const VADITAPGRAD = () => {
 
 
 
-export default VADITAPGRAD;
+export default DITAPSEPT2021;
