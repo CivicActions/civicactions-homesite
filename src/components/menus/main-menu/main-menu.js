88df441e792275b817e596d228ab2db7cad121af
Jennifer Aube <jenniferaube0510@gmail.com>
2022-10-31 13:12:51 -0400

    updating header menu styling and opening sub menu item

--- src/components/menus/main-menu/main-menu.js
+++ src/components/menus/main-menu/main-menu.js
@@ -4,39 +4,14 @@ import PropTypes from 'prop-types'
 
 const MainMenu = ({ redHeader }) => {
 
-  const headingsRef = useRef([]);
-  const onCollapseClick = (index) => {
-    const current = headingsRef.current[index].className;
-
-    // Close open sub-menus.
-    const keys = Object.keys(headingsRef.current);
-    keys.forEach(function (value) {
-      headingsRef.current[value].className = 'menu--list__item'
-    })
-
-    // Add/remove the expanded class from the currently clicked item.
-    headingsRef.current[index].className = current.includes('expanded')
-      ? 'menu--list__item'
-      : 'menu--list__item expanded';
-
-    if (current.includes('expanded')) {
-      headingsRef.current[index].setAttribute('aria-expanded', 'false')
-    } else {
-      headingsRef.current[index].setAttribute('aria-expanded', 'true')
-    }
-  };
-
   return (
     <nav className='header-nav'>
       <ul className="menu--list">
         <li
-          className='menu--list__item'
-          ref={(e) => (headingsRef.current[0] = e)}>
+          className='menu--list__item'>
           <button
             className="heading"
-            onKeyDown={() => { }}
             tabIndex={0}
-            onClick={() => onCollapseClick(0)}
           >
             Company
           </button>
@@ -47,13 +22,10 @@ const MainMenu = ({ redHeader }) => {
           </ul>
         </li>
         <li
-          className='menu--list__item'
-          ref={(e) => (headingsRef.current[1] = e)}>
+          className='menu--list__item'>
           <button
             className="heading"
-            onKeyDown={() => { }}
             tabIndex={0}
-            onClick={() => onCollapseClick(1)}
           >
             Services
           </button>
@@ -68,13 +40,10 @@ const MainMenu = ({ redHeader }) => {
           </ul>
         </li>
         <li
-          className='menu--list__item'
-          ref={(e) => (headingsRef.current[2] = e)}>
+          className='menu--list__item'>
           <button
             className="heading"
-            onKeyDown={() => { }}
             tabIndex={0}
-            onClick={() => onCollapseClick(2)}
           >
             Our Work
           </button>
@@ -86,7 +55,7 @@ const MainMenu = ({ redHeader }) => {
         <li className='menu--list__item'><a href='https://medium.com/civicactions'>Insights</a></li>
         <li className='menu--list__item'><Link to='/careers/'>Careers</Link></li>
         <li className='menu--list__item'>{redHeader ?
-          <Link to='/contact' className='primary-button'>Contact us</Link>
+          <Link to='/contact'><div className='primary-button'>Contact us</div></Link>
           :
           <Link to='/contact'><div className='primary-button'>Contact us</div></Link>
         }</li>
