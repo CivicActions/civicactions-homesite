e34c4c05795a27860fdb4e2dfa4b2acd3b4ee07d
KETF <brenton.kelly@civicactions.com>
2024-01-17 14:45:27 -0500

    Removed instagram icon and updated twitter icon in footer.

--- src/components/social-icons/social-icon.js
+++ src/components/social-icons/social-icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SocialIcon = ({ index, link, icon, iconHover, alt }) => (
-  <a className={index} href={link} rel="nofollow" className="social-icon__item">
+const SocialIcon = ({ link, icon, iconHover, alt, platform }) => (
+  <a href={link} rel="nofollow" className={`social-icon__item ${platform}`}>
     <img className='black-icon' src={icon} alt={alt}></img>
       <img className='blue-icon' src={iconHover} alt={alt}></img>
   </a>
