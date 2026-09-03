a8dfbc9973e8b2f04ed68bb6cbe413b97d9e8674
Jacqueline <jacqueline.quiuntanilla@civicactions.com>
2026-09-03 16:20:43 -0500

    MAHOM 1154: Remove unneeded image alt text from mobile nav buttons.

--- src/components/header.js
+++ src/components/header.js
@@ -48,8 +48,8 @@ const Header = () => {
         right
         noTransition
         width={'75%'}
-        customBurgerIcon={<img alt='mobile menu navigation icon' src={menuIcon} />}
-        customCrossIcon={<img alt='mobile menu close button' src={closeIcon} />}
+        customBurgerIcon={<img alt='' src={menuIcon} />}
+        customCrossIcon={<img alt='' src={closeIcon} />}
       />
     </header>
 
