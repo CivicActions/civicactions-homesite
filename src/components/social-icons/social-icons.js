790a621d131b490f9fb2f999fb6f3ce174ea7953
Jacqueline <jacqueline.quiuntanilla@civicactions.com>
2026-09-16 11:18:26 -0500

    MAHOM-1155: Use list markup for social icon and bottom footer links.

--- src/components/social-icons/social-icons.js
+++ src/components/social-icons/social-icons.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import SocialIcon from './social-icon'
+import React from 'react';
+import SocialIcon from './social-icon';
 import { socialMediaData } from '../../data';
 
 const SocialIcons = () => {
   // This component contains data so that it can be used to place social media icons on any page.
   let socialIcons = socialMediaData.map((item, index) => {
-    let { link, alt, icon, iconHover, platform } = item
-    return <SocialIcon key={index} alt={alt} icon={icon} iconHover={iconHover} link={link} platform={platform} />
-  })
-  return (
-    <div className='social-icon'>
-      {socialIcons}
-    </div>
-  )
-}
+    let { link, alt, icon, iconHover, platform } = item;
+    return (
+      <SocialIcon
+        key={index}
+        alt={alt}
+        icon={icon}
+        iconHover={iconHover}
+        link={link}
+        platform={platform}
+      />
+    );
+  });
+  return <ul className='social-icon'>{socialIcons}</ul>;
+};
 
-export default SocialIcons
+export default SocialIcons;
